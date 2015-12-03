68286ca3fd122c5be2fef3d6d02ab21c8638d1b1
kickthecann <cannhein@gmail.com>
2015-12-03 15:24:33 -0800

    Loading Screen Updated

---
+++ External/Login Files/Loading/loadingFragment.java
@@ -0,0 +1,17 @@
+package com.parse.starter;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class loadingFragment extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.layout_loading, container, false);
+
+    }
+}
